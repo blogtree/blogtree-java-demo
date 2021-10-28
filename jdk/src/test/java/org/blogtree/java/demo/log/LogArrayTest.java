ca01398caea6edce1af42e4cd3b7b12b3b921513
AlanGeeker <alangeeker@outlook.com>
2021-10-28 08:15:29 +0800

    LogArrayTest

--- jdk/src/test/java/org/blogtree/java/demo/log/LogArrayTest.java
+++ jdk/src/test/java/org/blogtree/java/demo/log/LogArrayTest.java
@@ -9,11 +9,20 @@ import java.util.Arrays;
 public class LogArrayTest {
 
     @Test
-    public void test() {
+    public void testLog() {
         int[] array = new int[]{1, 2, 3, 4, 5};
         log.info("array = {}", array);
         log.info("Arrays.toString(array) = {}", Arrays.toString(array));
         // [main] INFO org.blogtree.java.demo.log.LogArrayTest - array = [1, 2, 3, 4, 5]
         // [main] INFO org.blogtree.java.demo.log.LogArrayTest - Arrays.toString(array) = [1, 2, 3, 4, 5]
+    }
+
+    @Test
+    public void test() {
+        int[] array = new int[]{1, 2, 3, 4, 5};
+        System.out.println(array);
+        System.out.println(Arrays.toString(array));
+        // [I@1efed156
+        // [1, 2, 3, 4, 5]
     }
 }
