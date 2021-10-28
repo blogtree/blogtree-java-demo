d1861a926a8f3ee68656f341c7b10ab743b0718d
AlanGeeker <alangeeker@outlook.com>
2021-10-28 08:12:57 +0800

    LogArrayTest LogListTest

---
+++ jdk/src/test/java/org/blogtree/java/demo/log/LogArrayTest.java
@@ -0,0 +1,19 @@
+package org.blogtree.java.demo.log;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+@Slf4j
+public class LogArrayTest {
+
+    @Test
+    public void test() {
+        int[] array = new int[]{1, 2, 3, 4, 5};
+        log.info("array = {}", array);
+        log.info("Arrays.toString(array) = {}", Arrays.toString(array));
+        // [main] INFO org.blogtree.java.demo.log.LogArrayTest - array = [1, 2, 3, 4, 5]
+        // [main] INFO org.blogtree.java.demo.log.LogArrayTest - Arrays.toString(array) = [1, 2, 3, 4, 5]
+    }
+}
---
+++ jdk/src/test/java/org/blogtree/java/demo/log/LogListTest.java
@@ -0,0 +1,18 @@
+package org.blogtree.java.demo.log;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Slf4j
+public class LogListTest {
+
+    @Test
+    public void test() {
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
+        log.info("list = {}", list);
+        // [main] INFO org.blogtree.java.demo.log.LogListTest - list = [1, 2, 3, 4, 5]
+    }
+}
